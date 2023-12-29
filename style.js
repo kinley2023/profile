fc776b0c59c2fc562a824eb5aeaac12f7b7db0e9
kinley2023 <155144782+kinley2023@users.noreply.github.com>
2023-12-29 16:24:36 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,48 @@
+function myFunction() {
+    var x = document.getElementById("myTopmenu");
+    if(x.className === "header"){
+      x.className += " respons";
+    } 
+    else{
+      x.className = "header";
+    }
+  }
+  let currentIndex = 0;
+function showSlide(index){ 
+    const slider = document.querySelector('.slider');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+if (index < 0) {
+    index = totalSlides - 1;
+}
+else if (index >= totalSlides) {
+    index = 0;
+}
+currentIndex = index;
+const translateValue = -index * 100 + '%';
+slider.style.transform = 'translateX(' + translateValue + ')';
+}
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplays) => {
+let startValue = 0;
+let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+let duration = Math.floor(interval / endValue);
+let counter =setInterval(function () {
+    startValue += 1;
+    valueDisplays.textContent = startValue;
+    if (startValue == endValue) {
+        clearInterval(counter);
+    }
+}, duration);
+
+});
